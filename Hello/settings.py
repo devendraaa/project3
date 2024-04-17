e4e089c2fe078215f66a4db9b632f0c59f82825a
This_is_dev <39517486+devendraaa@users.noreply.github.com>
2024-04-17 03:31:37 -0700

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_ROOT = BASE_DIR/'assets'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 
